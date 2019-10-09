3c335ea388671edea36d53871ecbb1bf362358c4
Mustafa Saeed <mustafasaeed202019@gmail.com>
2019-10-10 00:19:18 +0200

    Added basic Entities

---
+++ src/Task/Task.Entites/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task.Entites
+{
+    public class Category : BaseEntity
+    {
+        [Required]
+        [StringLength(150, ErrorMessage = "Name :  Length should not exceed 150 characters")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public virtual IList<Post> Posts { get; set; }
+    }
+}
---
+++ src/Task/Task.Entites/Post.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task.Entites
+{
+    public class Post : BaseEntity
+    {
+        [Required(ErrorMessage = "Title: Field is required")]
+        [StringLength(500, ErrorMessage = "Title: Length should not exceed 500 characters")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "ShortDescription: Field is required")]
+        [Display(Name = "Short Description")]
+        public string ShortDescription { get; set; }
+
+        [Required(ErrorMessage = "Description: Field is required")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Image Url Is Required")]
+        [Display(Name = "Image Url")]
+        public string ImageUrl { get; set; }
+        public Category Category { get; set; }
+    }
+}
