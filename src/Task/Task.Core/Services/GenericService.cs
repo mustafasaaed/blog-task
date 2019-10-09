1c73857b99e662304ab8fe91ee280bae226db78c
Mustafa Saeed <mustafasaeed202019@gmail.com>
2019-10-10 00:26:46 +0200

    Added Generic implementation of IGenericService

---
+++ src/Task/Task.Core/Services/GenericService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Task.Core.Interfaces;
+using Task.Entites;
+
+namespace Task.Core.Services
+{
+    public class GenericService<TEntity> : IGenericService<TEntity> where TEntity : BaseEntity
+    {
+        private readonly IRepository<TEntity> _repository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GenericService(IRepository<TEntity> repository, IUnitOfWork unitOfWork)
+        {
+            _repository = repository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public TEntity GetById(int id)
+        {
+            if (id == 0)
+                return null;
+            return _repository.GetById(id);
+        }
+
+        public IEnumerable<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = "")
+        {
+            return _repository.Get(filter, orderBy, includeProperties);
+        }
+
+        public void Insert(TEntity entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+            _repository.Insert(entity);
+            _unitOfWork.Complete();
+        }
+
+
+        public void Update(TEntity entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+            _unitOfWork.Complete();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+            _repository.Delete(entity);
+            _unitOfWork.Complete();
+        }
+    }
+}
