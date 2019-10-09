922d1cbe89ff0b3dd041ddcc388467436162c5ae
Mustafa Saeed <mustafasaeed202019@gmail.com>
2019-10-10 00:50:38 +0200

    Added  IApplicationDbContext

---
+++ src/Task/Task.Data/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task.Data.Interfaces
+{
+    public interface IApplicationDbContext : IDisposable
+    {
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+        DbSet Set(Type entityType);
+        int SaveChanges();
+        IEnumerable<DbEntityValidationResult> GetValidationErrors();
+        DbEntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
+        DbEntityEntry Entry(object entity);
+        string ConnectionString { get; set; }
+        bool AutoDetectChangedEnabled { get; set; }
+        void ExecuteSqlCommand(string p, params object[] o);
+        void ExecuteSqlCommand(string p);
+        Database Database { get; }
+        DbChangeTracker ChangeTracker { get; }
+        DbContextConfiguration Configuration { get; }
+        Type GetType();
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+        Task<int> SaveChangesAsync();
+        void Dispose(bool disposing);
+        void OnModelCreating(DbModelBuilder modelBuilder);
+        bool ShouldValidateEntity(DbEntityEntry entityEntry);
+        DbEntityValidationResult ValidateEntity(DbEntityEntry entityEntry, IDictionary<object, object> items);
+    }
+}
