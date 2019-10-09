00d03d472e586bfdd6efdc308946c8261e7a6fd3
Mustafa Saeed <mustafasaeed202019@gmail.com>
2019-10-10 01:26:51 +0200

    Added ApplicationUser and ApplicationDbContext

---
+++ src/Task/Task.Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task.Entites;
+
+namespace Task.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+                 : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+    }
+}
