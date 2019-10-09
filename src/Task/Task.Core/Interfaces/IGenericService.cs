4b9839786ca9bc865688eb0831bcbce282f926c8
Mustafa Saeed <mustafasaeed202019@gmail.com>
2019-10-10 00:24:06 +0200

    Added IUnitOfWork , IRepository and IGenericService

---
+++ src/Task/Task.Core/Interfaces/IGenericService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Task.Entites;
+
+namespace Task.Core.Interfaces
+{
+    public interface IGenericService<TEntity> where TEntity : BaseEntity
+    {
+        IEnumerable<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = "");
+        TEntity GetById(int id);
+        void Insert(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
