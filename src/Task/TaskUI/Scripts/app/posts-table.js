176bf75b0f679c98e3d7ba99d61a215539295e2f
Mustafa Saeed <mustafasaeed202019@gmail.com>
2019-10-10 22:16:00 +0200

    Added filteration logic to posts table

---
+++ src/Task/TaskUI/Scripts/app/posts-table.js
@@ -0,0 +1,9 @@
+﻿$(function () {
+
+    $("#filter").change(function () {
+        var value = $(this).val();
+        $("td.categoryColumn:contains('" + value + "')").parent().show();
+        $("td.categoryColumn:not(:contains('" + value + "'))").parent().hide();
+    });
+
+});
