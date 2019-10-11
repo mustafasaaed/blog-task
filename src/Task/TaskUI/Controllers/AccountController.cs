0c2ab1a224b8e5183699e6718d4aa86a5a7c2b62
Mustafa Saeed <mustafasaeed202019@gmail.com>
2019-10-11 21:44:03 +0200

    Replaced all calls to home controller with posts controller

--- src/Task/TaskUI/Controllers/AccountController.cs
+++ src/Task/TaskUI/Controllers/AccountController.cs
@@ -165,7 +165,7 @@ namespace TaskUI.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "Posts");
                 }
                 AddErrors(result);
             }
@@ -394,7 +394,7 @@ namespace TaskUI.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Posts");
         }
 
         //
@@ -473,7 +473,7 @@ namespace TaskUI.Controllers
             {
                 return Redirect(returnUrl);
             }
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Posts");
         }
 
         internal class ChallengeResult : HttpUnauthorizedResult
